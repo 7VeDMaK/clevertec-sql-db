4a6c20d65ebf1b29938795fb40b0b0b5c89a5f47
Balay D. <darvinbin@gmail.com>
2024-09-26 21:49:55 +0300

    Task 1

---
+++ query.sql
@@ -0,0 +1,8 @@
+--Task 1
+
+-- select aircrafts_data.model, fare_conditions, count(*) as amount
+-- from seats
+--          join
+--      aircrafts_data on aircrafts_data.aircraft_code = seats.aircraft_code
+-- group by model, fare_conditions
+-- order by model, fare_conditions DESC
