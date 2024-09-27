76d9654071883202b28c62cc2e1e9afa6b17100c
Balay D. <darvinbin@gmail.com>
2024-09-27 16:58:23 +0300

    Task 4

--- query.sql
+++ query.sql
@@ -4,7 +4,7 @@
 -- select model ->> 'en' as model, fare_conditions, count(*) as amount
 -- from seats
 --          join
---      aircrafts_data on aircrafts_data.aircraft_code = seats.aircraft_code
+--      aircrafts_data using(aircraft_code)
 -- group by model, fare_conditions
 -- order by model, fare_conditions DESC
 
@@ -13,7 +13,7 @@
 -- select model ->> 'en' as model, count(*) as amount
 -- from seats
 --          join
---      aircrafts_data on aircrafts_data.aircraft_code = seats.aircraft_code
+--      aircrafts_data using(aircraft_code)
 -- group by model
 -- order by amount DESC
 -- limit 3
@@ -27,4 +27,13 @@
 
 --Task 4
 
+-- select passenger_name, contact_data, fare_conditions, book_date
+-- from ticket_flights
+--          join tickets using (ticket_no)
+--          join bookings using (book_ref)
+-- where fare_conditions = 'Business'
+-- order by book_date DESC
+-- limit 10
+
+--Task 5
 
