3a2a8238851b459a61b7e966ed611d022de25497
Balay D. <darvinbin@gmail.com>
2024-09-27 12:46:29 +0300

    Tasks 2, 3

--- query.sql
+++ query.sql
@@ -1,8 +1,30 @@
+-- http://localhost:5050
 --Task 1
 
--- select aircrafts_data.model, fare_conditions, count(*) as amount
+-- select model ->> 'en' as model, fare_conditions, count(*) as amount
 -- from seats
 --          join
 --      aircrafts_data on aircrafts_data.aircraft_code = seats.aircraft_code
 -- group by model, fare_conditions
 -- order by model, fare_conditions DESC
+
+--Task 2
+
+-- select model ->> 'en' as model, count(*) as amount
+-- from seats
+--          join
+--      aircrafts_data on aircrafts_data.aircraft_code = seats.aircraft_code
+-- group by model
+-- order by amount DESC
+-- limit 3
+
+--Task 3
+
+-- select *
+-- from flights
+-- where actual_departure - scheduled_departure > interval '2 hours'
+-- order by actual_departure - scheduled_departure
+
+--Task 4
+
+
